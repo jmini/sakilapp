9836174e6c86b2afda16dc68ca6772cb43c2a417
jmini <dev@jmini.fr>
2011-10-25 07:37:28 +0200

    Add OutlineService for Customers and Retail

---
+++ scout/net.sakilapp.server/src/net/sakilapp/server/services/outline/CustomersOutlineService.java
@@ -0,0 +1,7 @@
+package net.sakilapp.server.services.outline;
+
+import org.eclipse.scout.service.AbstractService;
+import net.sakilapp.shared.services.outline.ICustomersOutlineService;
+
+public class CustomersOutlineService extends AbstractService implements ICustomersOutlineService{
+}
---
+++ scout/net.sakilapp.shared/src/net/sakilapp/shared/services/outline/ICustomersOutlineService.java
@@ -0,0 +1,6 @@
+package net.sakilapp.shared.services.outline;
+
+import org.eclipse.scout.service.IService;
+
+public interface ICustomersOutlineService extends IService{
+}
